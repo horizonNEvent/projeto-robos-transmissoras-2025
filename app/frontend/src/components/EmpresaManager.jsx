59d8df85763c60cab0321267286ccbcdc6aff304
horizonNEvent <dibu695@gmail.com>
2026-01-07 12:20:06 -0300

    Correção final: API dinâmica em todos os componentes

--- app/frontend/src/components/EmpresaManager.jsx
+++ app/frontend/src/components/EmpresaManager.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = "http://localhost:8000"
+const API_URL = `http://${window.location.hostname}:8000`
 
 function EmpresaManager({ empresas, onUpdate, onLog, formData, setFormData, editingId, setEditingId }) {
 
--- app/frontend/src/components/SigetPublicManager.jsx
+++ app/frontend/src/components/SigetPublicManager.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API_URL = "http://localhost:8000"
+const API_URL = `http://${window.location.hostname}:8000`
 
 export default function SigetPublicManager({ onLog }) {
     const [targets, setTargets] = useState([])
