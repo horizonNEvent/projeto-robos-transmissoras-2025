ceb47adbde0d7465f47e13e94b1bc048dfdbe9d1
horizonNEvent <dibu695@gmail.com>
2026-01-07 13:18:38 -0300

    Desativa checagem de host no Vite para compatibilidade total

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all'
+    allowedHosts: true,
+    host: true,
+    hmr: {
+      clientPort: 443
+    }
   }
 })
