095aa169e046919ba5b0928c0a4ccbe03ca316c5
horizonNEvent <dibu695@gmail.com>
2026-04-07 20:21:26 -0300

    fix: adiciona configuracao de ROBOT_PYTHON no start_all.bat

--- start_all.bat
+++ start_all.bat
@@ -12,6 +12,12 @@ echo [1/3] Atualizando codigo (git pull)...
 git pull
 echo.
 
+REM --- Configuracao do Python para os Robos ---
+REM Se este PC tiver Python 3.11 instalado, descomente a linha abaixo:
+REM set ROBOT_PYTHON=C:\Python311\python.exe
+REM Se a variavel nao for definida, o backend usa "python" como padrao.
+REM --------------------------------------------
+
 echo [2/3] Iniciando Ngrok em segundo plano...
 start "NGROK TUNNEL" ngrok http 8000
 timeout /t 3 /nobreak > nul
